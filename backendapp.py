e99b17efd86e51586fd246201a898a58ec173c77
moweb24 <165305658+moweb24@users.noreply.github.com>
2025-10-12 11:17:52 +0530

    Add files via upload

---
+++ backendapp.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Flask running successfully"
+
+@app.route('/api')
+def api():
+    return jsonify({"message":"Initial API response"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
